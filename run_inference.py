8631579b16c27aff76408edd0564fa1b16677adf
Pragya Khanna <75221153+pragyakhanna11@users.noreply.github.com>
2024-03-25 19:26:16 +0530

    testing

--- run_inference.py
+++ run_inference.py
@@ -7,7 +7,8 @@ from pytorch_lightning import seed_everything
 from cldm.model import create_model, load_state_dict
 from cldm.ddim_hacked import DDIMSampler
 from cldm.hack import disable_verbosity, enable_sliced_attention
-from datasets.data_utils import * 
+# from datasets.data_utils import * 
+from data_utils import * 
 cv2.setNumThreads(0)
 cv2.ocl.setUseOpenCL(False)
 import albumentations as A
